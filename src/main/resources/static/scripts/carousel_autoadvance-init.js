629f319ea378db3095aeebc4bf30abafb1783f5c
Unknown <sapphon@gmail.com>
2018-12-10 17:28:08 -0500

    Remove js scripts related to Materialize carousel (unused)

--- src/main/resources/static/scripts/carousel_autoadvance-init.js
+++
@@ -1,4 +0,0 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {fullWidth: true, indicators: true});
-  });
